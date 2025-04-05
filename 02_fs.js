bf178fcb3a779629a6667c8a1108fe03e888b70b
miguelmanabo4 <miguelmanabo4@gmail.com>
2025-04-05 05:14:51 +0000

    Laboratory Midterm Exam

--- 02_fs.js
+++ 02_fs.js
@@ -5,40 +5,41 @@ fs.writeFile('message.txt', "Node.js is awesome!", (err) => {
         console.log("Error in creating file")
     } else {
         console.log("File created Successfullly")
+        fs.access('message.txt', fs.constants.F_OK, (err) => {
+            if (err) {
+                console.log("File does not exist!")
+            } else {
+                fs.readFile('message.txt', 'utf8', (err, data) => {
+                    if (err) {
+                        console.log("error in reading the file")
+                    } else {
+                        console.log(data)
+                        fs.access('message.txt', fs.constants.F_OK, (err) => {
+                            if (err) {
+                                console.log("File does not exist!")
+                            } else {
+                                fs.unlink('message.txt', (err) => {
+                                    if (err) {
+                                        console.log("error in deleting the file")
+                                    } else {
+                                        console.log("File deleted successfully")
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+        })
     }
 })
 
-setTimeout(() => {
-    fs.access('message.txt', fs.constants.F_OK, (err) => {
-        if (err) {
-            console.log("File does not exist!")
-        } else {
-            fs.readFile('message.txt', 'utf8', (err, data) => {
-                if (err) {
-                    console.log("error in reading the file")
-                } else {
-                    console.log(data)
-                }
-            })
-        }
-    })
-}, 1000)
 
-setTimeout(() => {
-    fs.access('message.txt', fs.constants.F_OK, (err) => {
-        if (err) {
-            console.log("File does not exist!")
-        } else {
-            fs.unlink('message.txt', (err) => {
-                if (err) {
-                    console.log("error in deleting the file")
-                } else {
-                    console.log("File deleted successfully")
-                }
-            })
-        }
-    })
-}, 2000)
+
+
+
+
+
 
 
 
--- 03_url.js
+++ 03_url.js
@@ -3,8 +3,15 @@ const url = require('url');
 const myUrl = 'https://www.example.com:8080/products?category=electronics&sort=price';
 const parsedUrl = url.parse(myUrl, true);
 
+// Logging various components of the URL
 console.log('Protocol:', parsedUrl.protocol);
 console.log('Hostname:', parsedUrl.hostname);
 console.log('Port:', parsedUrl.port);
 console.log('Pathname:', parsedUrl.pathname);
-console.log('Query Parameters:', parsedUrl.query.category + " " + parsedUrl.query.sort);
+const standardQuery = Object.assign({}, parsedUrl.query)
+// console.log('Query Parameters:', Object.assign({}, parsedUrl.query).toString());
+console.log('Query Parameters:', JSON.stringify(parsedUrl.query))
+// console.log('Query Parameters:', "{ category: " + parsedUrl.query.category + ", sort: " + parsedUrl.query.sort + " }")
+
+
+
